63dfce90464a345f82a5b73a134c116c545f7685
Celal Gündoğdu <celalgundogdu42@gmail.com>
2023-07-31 22:18:02 +0300

    fix jwt exception

--- src/main/java/com/project/blogapp/security/JwtAuthenticationFilter.java
+++ src/main/java/com/project/blogapp/security/JwtAuthenticationFilter.java
@@ -1,8 +1,12 @@
 package com.project.blogapp.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.blogapp.constant.Constants;
+import com.project.blogapp.exception.BusinessException;
 import com.project.blogapp.repository.TokenRepository;
 import com.project.blogapp.util.JwtService;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,6 +21,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -39,22 +48,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         final String jwt;
         final String username;
-        if (authorizationHeader != null && authorizationHeader.startsWith(Constants.BEARER_PREFIX)) {
-            jwt = authorizationHeader.substring(7);
-            username = jwtService.extractUsername(jwt);
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-                if (isTokenValid(jwt, userDetails)) {
-                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                            userDetails,
-                            null,
-                            userDetails.getAuthorities()
-                    );
-                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        try {
+            if (authorizationHeader != null && authorizationHeader.startsWith(Constants.BEARER_PREFIX)) {
+                jwt = authorizationHeader.substring(7);
+                username = jwtService.extractUsername(jwt);
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+                    if (isTokenValid(jwt, userDetails)) {
+                        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+                        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    }
                 }
             }
+        } catch (JwtException exception) {
+            response.setContentType(APPLICATION_JSON_VALUE);
+            response.setStatus(FORBIDDEN.value());
+            response.setHeader("error", exception.getMessage());
+            final Map<String,String> error = new HashMap<>();
+            error.put("error",exception.getMessage());
+            new ObjectMapper().writeValue(response.getOutputStream(),error);
         }
+
         filterChain.doFilter(request, response);
     }
 
